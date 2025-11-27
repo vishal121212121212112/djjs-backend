025da801c01652660f3410646134742e8f15a15d
Vaishnavi Sinha <vaishnavisinha2903@gmail.com>
2025-11-27 23:48:18 +0530

    Add validation for volunteer related apis

--- app/models/volunteer.go
+++ app/models/volunteer.go
@@ -6,14 +6,13 @@ import "time"
 // swagger:model Volunteer
 type Volunteer struct {
 	ID            uint       `gorm:"primaryKey;autoIncrement" json:"id"`
-	BranchID      uint       `gorm:"not null" json:"branch_id"`
+	BranchID      uint       `gorm:"not null" json:"branch_id" validate:"required,min=1"`
 	Branch        Branch     `gorm:"foreignKey:BranchID" json:"branch,omitempty"`
-	SearchValue   string     `gorm:"column:search_volunteer" json:"search_volunteer,omitempty"`
-	VolunteerName string     `gorm:"not null" json:"volunteer_name"`
-	NumberOfDays  int        `gorm:"column:number_of_days" json:"number_of_days,omitempty"`
-	SevaInvolved  string     `json:"seva_involved,omitempty"`
-	MentionSeva   string     `gorm:"column:mention_seva" json:"mention_seva,omitempty"`
-	EventID       uint       `json:"event_id"`
+	VolunteerName string     `gorm:"not null" json:"volunteer_name" validate:"required,min=2,max=255"`
+	NumberOfDays  int        `gorm:"column:number_of_days" json:"number_of_days,omitempty" validate:"omitempty,min=0,max=365"`
+	SevaInvolved  string     `json:"seva_involved,omitempty" validate:"omitempty,min=2,max=500"`
+	MentionSeva   string     `gorm:"column:mention_seva" json:"mention_seva,omitempty" validate:"omitempty,min=2,max=500"`
+	EventID       uint       `json:"event_id" validate:"required,min=1"`
 	Event         Event      `gorm:"foreignKey:EventID;references:ID" json:"event,omitempty"`
 	CreatedOn     time.Time  `json:"created_on,omitempty"`
 	UpdatedOn     *time.Time `json:"updated_on,omitempty"`
